09537cb6102590e27f57b9d8fc2f6a4011e27867
kwando <kwando9874123@hotmail.com>
2020-12-09 22:26:20 -0800

    combo combo combo

---
+++ aoc_d10.py
@@ -0,0 +1,38 @@
+my_file = open("day10.txt", "r")
+content = my_file.readlines()
+
+voltages = []
+sorted_voltages = []
+one_away = 1
+three_away = 0
+trib = [1, 1, 2, 4, 7, 13]
+# #MakeMyBag
+
+for c in content:
+    voltages.append(int(c.strip("\n")))
+
+sorted_voltages = sorted(voltages)
+
+print sorted_voltages
+
+i = 0
+j = 0
+value = 1
+
+while i < len(sorted_voltages):
+    if i+1 < len(sorted_voltages) and sorted_voltages[i+1] - sorted_voltages[i] == 1:
+        #print "Voltage: " + str(sorted_voltages[i])
+        one_away = one_away + 1
+    else:
+        three_away = three_away + 1
+        value = value * trib[(i - j)]
+        print value
+        j = i + 1
+    i = i + 1
+
+print one_away
+print three_away
+print one_away * three_away
+print value # Tribonacci numbers. I don't want to write code to re-find this answer.
+#
+#1,3,2,1,1,2,1,1,1,1
